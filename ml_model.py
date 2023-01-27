044f00e4ee4b99f433c76a63744d1c0772b07546
sandeep12d3 <sandeep12d3@gmail.com>
2023-01-27 23:52:01 +0530

    added 3 new ml code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding all ml related code here")
+
+def model1():
+    print("Adding code for ml model 1")
+
+def model2():
+    print("Adding code for ml model 2")
+
+def model3():
+    print("Adding code for ml model 3")
